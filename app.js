b9f9f1d40021816280667eccd0350cc8af82451c
黄建峰 <645381995@qq.com>
2016-08-16 10:04:06 +0800

    'done'

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-// var express = require('express');
-// var cheerio = require('cheerio');
-// var superagent = require('superagent');
-
-// var app = express();
-
-// app.get('/'function (req, res, next) {
-//     superagent.get('http://cnodejs.org')
-//         .end(function (err, sres) {
-//             if (err) {
-//                 return next(err);
-//             }
-//             var $ = cheerio.load(sres.text);
-//             var items = [];
-//             $('#topic_list .topic_title').each(function (idx, element) {
-//                 var $element = $(element);
-//                 items.push({
-//                     title: $element.attr('title');
-//                     href: $element.attr('href');
-//                 });
-//             });
-//             res.send(items);
-//         })
-// })
-
 var express = require('express');
 var cheerio = require('cheerio');
 var superagent = require('superagent');
@@ -44,7 +19,6 @@ app.get('/', function (req, res, next) {
           href: $element.attr('href')
         });
       });
-
       res.send(items);
     });
 });
